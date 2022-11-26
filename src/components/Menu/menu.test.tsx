b061698b7740bb76ed68619a6cda453e4e4fa721
xing-6464 <xing_6464@foxmail.com>
2022-11-26 15:32:38 +0800

    测试驱动-Menu测试添加

---
+++ src/components/Menu/menu.test.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { render, RenderResult, screen, fireEvent } from '@testing-library/react'
+
+import Menu, { MenuProps } from './menu'
+import MenuItem from './menuItem'
+
+const testProps: MenuProps = {
+  defaultIndex: 0,
+  onSelect: jest.fn(),
+  className: 'test',
+}
+
+const testVerProps: MenuProps = {
+  defaultIndex: 0,
+  mode: 'vertical'
+}
+
+const generateMenu = (props: MenuProps) => {
+  return (
+    <Menu {...props}>
+      <MenuItem index={0}>
+        active
+      </MenuItem>
+      <MenuItem index={1} disabled>
+        disabled
+      </MenuItem>
+      <MenuItem index={2}>
+        index2
+      </MenuItem>
+    </Menu>
+  )
+}
+let menuElement: HTMLElement, activeElement: HTMLElement, disabledElement: HTMLElement
+const setup = (props: MenuProps) => {
+  render(generateMenu(props)) as RenderResult
+  menuElement = screen.getByTestId('test-menu')
+  activeElement = screen.getByText('active')
+  disabledElement = screen.getByText('disabled')
+}
+describe('tset Menu and MenuItem component', () => {
+  it('should render correct Menu and MenuItem based on default props', () => {
+    setup(testProps)
+    expect(menuElement).toBeInTheDocument()
+    expect(menuElement).toHaveClass('star-menu test')
+    expect(menuElement.getElementsByTagName('li').length).toEqual(3)
+    expect(activeElement).toHaveClass('menu-item is-active')
+    expect(disabledElement).toHaveClass('menu-item is-disabled')
+  })
+
+  it('click items should change active and call the right callback', () => {
+    setup(testProps)
+    const thirdItem = screen.getByText('index2')
+
+    fireEvent.click(thirdItem)
+    expect(thirdItem).toHaveClass('menu-item is-active')
+    expect(menuElement).not.toHaveClass('is-active')
+    expect(testProps.onSelect).toHaveBeenCalledWith(2)
+
+    fireEvent.click(disabledElement)
+    expect(disabledElement).not.toHaveClass('is-active')
+    expect(testProps.onSelect).not.toHaveBeenCalledWith(1)
+  })
+
+  it('should render vertical mode when mode is set to vertical', () => {
+    setup(testVerProps)
+    expect(menuElement).toHaveClass('menu-vertical')
+  })
+})
