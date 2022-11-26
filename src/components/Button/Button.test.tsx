ae372021ba8b7c6aa9986ee58beb420a66d6669a
xing-6464 <xing_6464@foxmail.com>
2022-11-26 12:45:04 +0800

    添加Button 测试代码 第一部分

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -2,9 +2,23 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import Button from './button'
 
-test('our first react test case', () => {
-  render(<Button>Nice</Button>)
-  const element = screen.queryByText('Nice')
 
-  expect(element).toBeTruthy()
+describe('test Button component', () => {
+  it('should render the correct default button', () => {
+    render(<Button>Nice</Button>)
+    const element = screen.getByText('Nice')
+
+    expect(element).toBeInTheDocument()
+    expect(element.tagName).toEqual('BUTTON')
+    expect(element).toHaveClass('btn btn-default')
+  })
+  it('should render the correct component based on different props', () => {
+
+  })
+  it('should render a link when btnType equals link and href is provided', () => {
+
+  })
+  it('should render disabled button when disabled set to true', () => {
+
+  })
 })
