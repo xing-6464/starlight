8e382289842b47895a34414e88925054a9403a62
xing-6464 <xing_6464@foxmail.com>
2022-11-26 12:03:09 +0800

    Alert组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import Button, { ButtonSize, ButtonType } from './components/Button/button'
+// import Button, { ButtonSize, ButtonType } from './components/Button/button'
+import Alert from './components/Alert'
 
 const App: React.FC = () => {
   return (
-    <>
-      <Button className='custom'>Hello</Button>
-      <Button btnType={ButtonType.Primary} size={ButtonSize.Large} disabled>Hello</Button>
-      <Button btnType={ButtonType.Danger} size={ButtonSize.Small} >Hello</Button>
-      <Button btnType={ButtonType.Link} href='https://www.baidu.com' target='_blank'>西瓜</Button>
-    </>
+    <Alert title='星'></Alert>
   )
 }
 
---
+++ src/components/Alert/alert.tsx
@@ -0,0 +1,59 @@
+import React, { FC, useState } from 'react'
+import classNames from 'classnames'
+export type AlertType = 'success' | 'default' | 'danger' | 'warning'
+
+export interface AlertProps {
+  /**标题 */
+  title: string;
+  /**描述 */
+  description?: string;
+  /**类型 四种可选 针对四种不同的场景 */
+  type?: AlertType;
+  /**关闭alert时触发的事件 */
+  onClose?: () => void;
+  /**是否显示关闭图标*/
+  closable?: boolean;
+}
+
+/** 
+ * 用于页面中展示重要的提示信息。 点击右侧的叉提示自动消失
+ * ### 引用方法
+ * 
+ * ~~~js
+ * import { Alert } from 'starlight'
+ * ~~~
+*/
+export const Alert: FC<AlertProps> = (props) => {
+  const [ hide, setHide ] = useState(false)
+  const {
+    title,
+    description,
+    type = 'default',
+    onClose,
+    closable = true
+  } = props
+
+  const classes = classNames('star-alert', {
+    [`star-alert-${type}`]: type,
+  })
+  const titleClass = classNames('star-alert-title', {
+    'bold-title': description
+  })
+  const handleClose = (e: React.MouseEvent) => {
+    if (onClose) {
+      onClose()
+    }
+    setHide(true)
+  }
+  return (
+    <>{ !hide ? (
+        <div className={classes}>
+          <span className={titleClass}>{title}</span>
+          {description && <p className="star-alert-desc">{description}</p>}
+          {closable && <span className="star-alert-close" onClick={handleClose}>关闭</span>}
+        </div> ) : ('') }
+    </>
+  )
+}
+
+export default Alert
