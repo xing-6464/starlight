0193057e45a759cf3a67b14d95e6f278db6a3e19
xing-6464 <xing_6464@foxmail.com>
2022-11-26 13:05:08 +0800

    添加Button 测试代码 第二部分

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,24 +1,58 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
-import Button from './button'
+import { render, screen, fireEvent } from '@testing-library/react'
+import Button, { ButtonProps, ButtonSize, ButtonType } from './button'
 
 
+const defaultProps = {
+  onClick: jest.fn()
+}
+const testProps: ButtonProps = {
+  btnType: ButtonType.Primary,
+  size: ButtonSize.Large,
+  className: 'class'
+}
+const disabledProps: ButtonProps = {
+  disabled: true,
+  onClick: jest.fn()
+}
 describe('test Button component', () => {
+
   it('should render the correct default button', () => {
-    render(<Button>Nice</Button>)
+    render(<Button {...defaultProps}>Nice</Button>)
+    const element = screen.getByText('Nice') as HTMLButtonElement
+    expect(element).toBeInTheDocument()
+
+    expect(element.tagName).toEqual('BUTTON')
+    expect(element.disabled).toBeFalsy()
+    expect(element).toHaveClass('btn btn-default')
+
+    fireEvent.click(element)
+    expect(defaultProps.onClick).toHaveBeenCalled()
+  })
+
+  it('should render the correct component based on different props', () => {
+    render(<Button {...testProps}>Nice</Button>)
     const element = screen.getByText('Nice')
+    expect(element).toBeInTheDocument()
+
+    expect(element).toHaveClass('btn btn-primary btn-lg class')
+  })
+
+  it('should render a link when btnType equals link and href is provided', () => {
+    render(<Button btnType={ButtonType.Link} href='http://xing'>Link</Button>)
+    const element = screen.getByText('Link')
+    expect(element).toBeInTheDocument()
+    expect(element.tagName).toEqual('A')
+    expect(element).toHaveClass('btn btn-link')
+  })
+
+  it('should render disabled button when disabled set to true', () => {
+    render(<Button {...disabledProps}>Nice</Button>)
+    const element = screen.getByText('Nice') as HTMLButtonElement
 
     expect(element).toBeInTheDocument()
-    expect(element.tagName).toEqual('BUTTON')
-    expect(element).toHaveClass('btn btn-default')
-  })
-  it('should render the correct component based on different props', () => {
-
-  })
-  it('should render a link when btnType equals link and href is provided', () => {
-
-  })
-  it('should render disabled button when disabled set to true', () => {
-
+    expect(element.disabled).toBeTruthy()
+    fireEvent.click(element)
+    expect(disabledProps.onClick).not.toHaveBeenCalled()
   })
 })
